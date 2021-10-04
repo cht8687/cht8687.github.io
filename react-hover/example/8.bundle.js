414d509f0696b7d8352cfae97ad18eb2cf728013
Robert Chang <cht8687@gmail.com>
2021-10-04 13:54:08 +1100

    update react-hover

---
+++ react-hover/example/8.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{132:function(n,a,e){"use strict";function t(n){n.languages.arff={comment:/%.*/,string:{pattern:/(["'])(?:\\.|(?!\1)[^\\\r\n])*\1/,greedy:!0},keyword:/@(?:attribute|data|end|relation)\b/i,number:/\b\d+(?:\.\d+)?\b/,punctuation:/[{},]/}}n.exports=t,t.displayName="arff",t.aliases=[]}}]);
+//# sourceMappingURL=8.bundle.js.map
